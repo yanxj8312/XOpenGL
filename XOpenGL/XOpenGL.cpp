166c63789a1b76c034a8abe26c340a107f08af34
yanxj8312 <cn_yanxj@foxmail.com>
2020-06-16 09:03:40 +0800

    添加texture 显示

--- XOpenGL/XOpenGL.cpp
+++ XOpenGL/XOpenGL.cpp
@@ -44,11 +44,14 @@ int main()
 		#version 330\n
 		uniform float a;
 		layout(location = 0) in vec3 pos;
+		layout(location = 1) in vec3 uvPos;
 		out vec3 outPos;
+		out vec3 outUVPos;
 		void main() 
 		{
 			outPos = pos;
-			float _a = sin(a);
+			outUVPos = uvPos;
+			float _a = 1;
 			gl_Position = vec4(pos.x*_a,pos.y*_a,pos.z*_a,1.0);
 		}
 	);
@@ -57,12 +60,17 @@ int main()
 		#version 330\n
 		out vec4 rgbaColor;
 		in vec3 outPos;
+		in vec3 outUVPos;
 
-		uniform Sample2D texture;
+		uniform sampler2D text;
 
 		void main() 
 		{
+			//纹理坐标
+			vec2 uv = vec2(outUVPos.x,outUVPos.y);
+			vec4 color = texture(text,uv);
 			rgbaColor = vec4(outPos, 1.0);
+			rgbaColor = color;
 		}
 	);
 
@@ -80,6 +88,13 @@ int main()
 		-1.0,-1.0,0.0
 	};
 
+	float vertexUV[] = {
+		0.0,1.0,0.0,
+		1.0,1.0,0.0,
+		1.0,0.0,0.0,
+		0.0,0.0,0.0
+	};
+
 	unsigned int index[] = {
 		0,3,1,
 		1,3,2
@@ -87,11 +102,38 @@ int main()
 
 	XGLVAO* vao = new XGLVAO();
 	vao->AddVertex3D(vertex, 4, 0);
+	vao->AddVertex3D(vertex, 4, 1);
 	vao->SetIndex(index, 6);
 
 	XGLProgram* program = new XGLProgram(vertexShader, fragmentShader);
 
-	float aa = 0.0;
+	int imgWidth = 2;
+	int imgHeight = 2;
+	unsigned char imgData[]={
+		255,0,0,	0,255,0	,
+		0,0,255,	127,127,127
+	};
+
+	float aa = 0.0f;
+
+	GLuint textureId = 0;
+	glGenTextures(1,&textureId);
+
+	glBindTexture(GL_TEXTURE_2D,textureId);
+
+	//将图片数据传入texture
+	//texture setting 
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE);
+	//big or small setting 
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	//传入数据
+	glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,imgWidth,imgHeight,0,GL_RGB,GL_UNSIGNED_BYTE,imgData);
+
+	//将texture 数据传入shader
+
+
 	while (!glfwWindowShouldClose(window)) //循环处理窗口关闭事件
 	{
 		//TODO 绘制操作
@@ -102,8 +144,16 @@ int main()
 		//传入uniform变量 注意是在绘制的时候传入该值
 		GLuint uniforma = glGetUniformLocation(program->program,"a");
 		glUniform1f(uniforma,aa);
-		aa+=0.1;
+		aa+=0.003;
 
+		GLuint textureLoc = glGetUniformLocation(program->program,"text");
+
+		//激活0号纹理单元
+		glActiveTexture(GL_TEXTURE0);
+		//将0号纹理单元与纹理绑定
+		glBindTexture(GL_TEXTURE_2D,textureId);
+		//绑定的是纹理单元
+		glUniform1i(textureLoc,0);
 		//绘制 使用vertex顶点坐标方式
 		//glDrawArrays(GL_TRIANGLES, 0, 6);
 		//使用ebo的方式绘制顶点坐标
