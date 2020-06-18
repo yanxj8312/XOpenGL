799f93cdd311f5a76808c8c2a2933df5bf5eacd3
yanxj <cn_yanxj@foxmail.com>
2020-06-18 13:15:25 +0800

    自定义shader

--- XOpenGL/XOpenGL.cpp
+++ XOpenGL/XOpenGL.cpp
@@ -7,12 +7,17 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "XGL/XGL.hpp"
+#include "atlimage.h"
+#include <iostream>
+#include <fstream>
 
 #define STRINGIZE(x) #x
 #define SHADER(shader) "" STRINGIZE(shader)
 
 
 using namespace std;
+
+
 
 int main()
 {
@@ -38,7 +43,6 @@ int main()
 
 	//创建shader之前必须要先初始化glad
 	
-	//GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER); //创建片源着色器
 
 	char* vertexShader = R"(
 		#version 330
@@ -51,8 +55,8 @@ int main()
 		{
 			outPos = pos;
 			outUVPos = uvPos;
-			float _a = 1;
-			gl_Position = vec4(pos.x*_a,pos.y*_a,pos.z*_a,1.0);
+			//float _a = 1;
+			gl_Position = vec4(pos.x,pos.y,pos.z,1.0);
 		}
 	)";
 
@@ -61,15 +65,19 @@ int main()
 		out vec4 rgbaColor;
 		in vec3 outPos;
 		in vec3 outUVPos;
+		in vec2 outWandH;
 
+		//外部传入的变量都使用uniform表示
+		uniform int width;
+		uniform int height;
 		uniform sampler2D text;
-
 		void main() 
 		{
+			vec2 singleStepOffset = vec2(1.0/float(width),1.0/float(height));
 			//纹理坐标
 			vec2 uv = vec2(outUVPos.x,outUVPos.y);
 			vec4 color = texture(text,uv);
-			rgbaColor = vec4(outPos, 1.0);
+			//rgbaColor = vec4(outPos, 1.0);
 			rgbaColor = color;
 		}
 	)";
@@ -107,12 +115,30 @@ int main()
 
 	XGLProgram* program = new XGLProgram(vertexShader, fragmentShader);
 
-	int imgWidth = 2;
+	/*int imgWidth = 2;
 	int imgHeight = 2;
 	unsigned char imgData[]={
 		255,0,0,	0,255,0	,
 		0,0,255,	127,127,127
-	};
+	};*/
+
+	BITMAP bm;
+	CImage img;
+
+	HRESULT hr = img.Load("D:\\XSOURCE\\test.jpg");
+
+	if(!SUCCEEDED(hr))
+	{
+		printf("image file load failed\n");
+		return -1;
+	}
+
+	HBITMAP hbmp = img;
+
+	if(!GetObject(hbmp,sizeof(bm),&bm))
+		return -1;
+	
+
 
 	float aa = 0.0f;
 
@@ -128,11 +154,18 @@ int main()
 	//big or small setting 
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+	printf("hello world\n");
+	
 	//传入数据
-	glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,imgWidth,imgHeight,0,GL_RGB,GL_UNSIGNED_BYTE,imgData);
+	glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,bm.bmWidth,bm.bmHeight,0,GL_BGR,GL_UNSIGNED_BYTE,bm.bmBits);
+
+	GLuint widthLoc = glGetUniformLocation(program->program,"width");
+	glUniform1i(widthLoc,bm.bmWidth);
+
+	GLuint heightLoc = glGetUniformLocation(program->program,"height");
+	glUniform1i(heightLoc,bm.bmHeight);
 
 	//将texture 数据传入shader
-
 	while (!glfwWindowShouldClose(window)) //循环处理窗口关闭事件
 	{
 		//TODO 绘制操作
@@ -168,6 +201,6 @@ int main()
 
 	glfwTerminate();
 
-	cout << "hello world" << endl;
+	std::cout << "hello world" << endl;
 	return 0;
 }
